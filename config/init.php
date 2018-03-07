89620657d5b86f3171056d7cd22196e113f57baf
Romna Kislov <roman@kislovs.ru>
2018-03-07 23:52:24 +0500

    init.php

---
+++ config/init.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 07.03.2018
+ * Time: 23:30
+ *
+ * configuration file
+ */
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/ishop/core');
+define("LIBS", ROOT . '/vendor/ishop/core/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", ROOT . 'default');
+
+//http://ishop2.loc/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+//http://ishop2.loc/public/
+$app_path = preg_replace("#[^/]+$#",'',$app_path);
+//http://ishop2.loc/
+$app_path = str_replace('/public/','',$app_path);
+define("PATH", $app_path);
+define("ADMIN", PATH . '/admin');
+
+require_once  ROOT . '/vendor/autoload.php';
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 07.03.2018
+ * Time: 23:22
+ * Front controller
+ */
+
+require_once dirname(__DIR__ ). '/config/init.php';
